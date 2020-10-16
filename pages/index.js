7f10f8cdb2a8d263875db104178dc45a6698c8b7
Roman Sydoruk <sydoruk89@gmail.com>
2020-10-15 23:08:03 -0700

    api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+import axios from 'axios'
+
+const fetcher = async (url) => {
+  const response = await axios.get(url);
+  return response.data
+}
 
 export default function Home() {
+  const apiUrl = 'https://swapi.dev/api/people/'
+  const { data, error } = useSWR(apiUrl, fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
   return (
     <div className={styles.container}>
       <Head>
@@ -10,9 +22,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          This is my stocks
-          </h1>
+        {data.results.map((item)=>{
+          return <h2 key={item.url}>Hi, I am {item.name}</h2>
+        })}
+        <h1 className={styles.title}>This is my stocks</h1>
       </main>
     </div>
   )
